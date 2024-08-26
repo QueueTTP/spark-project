6b5361f8858c193164683ff2033c7557ab82bb7b
Teddy Archibald <107772813+teddyvere@users.noreply.github.com>
2024-08-25 22:42:02 -0400

    clean up

--- config_template.py
+++
@@ -1,8 +0,0 @@
-import mysql.connector
-
-db_config = mysql.connector.connect(
-                host="localhost",
-                user="root",
-                password="new_password",
-                database="million_songs"
-            )       
